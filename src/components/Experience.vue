517f3a319f47b19b95fc6773e851065f94be977f
Mark Mustard <mark.mustard@ge.com>
2020-03-06 21:35:14 -0500

    exo changes

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -5,23 +5,56 @@
         <v-card>
           <v-card-title>Experience</v-card-title>
           <v-list>
+
             <v-list-item>
               <v-list-item-avatar tile>
                 <v-img src="@/assets/ingage.png"></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title>Ingage Partners</v-list-item-title>
+                <v-list-item-title>Apprentice Developer - Ingage Partners</v-list-item-title>
                 <v-list-item-subtitle>August 2019 - Present</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
+
+            <v-list-item>
+              <v-list-item-avatar tile>
+                <v-img src="@/assets/TQL.png"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>Logistics Account Executive - Total Quality Logistics</v-list-item-title>
+                <v-list-item-subtitle>April 2018 - February 2019</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-avatar tile>
+                <v-img src="@/assets/TSC.png"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>Pick, Pack, and Ship Supervisor - TSC Apparel</v-list-item-title>
+                <v-list-item-subtitle>June 2014 - April 2018</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
           </v-list>
         </v-card>
       </v-col>
-    </v-row>
-    <v-row fluid>
-      <v-col fluid>
+      <v-col fluid class="col">
         <v-card>
           <v-card-title>Education</v-card-title>
+          <v-list>
+
+            <v-list-item>
+              <v-list-item-avatar tile>
+                <v-img src="@/assets/TE.png"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>Tech Elevator Bootcamp</v-list-item-title>
+                <v-list-item-subtitle>May 2019 - August 2019</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list>
         </v-card>
       </v-col>
     </v-row>
